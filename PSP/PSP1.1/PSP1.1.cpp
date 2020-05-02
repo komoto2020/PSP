38204ee79a50470ac7f8ec7deb8926124292214c
tkmthrkz <tkmthrkz@gmail.com>
2020-05-02 20:32:53 +0900

    PSP1.1プログラム完成（コンパイル、テスト済み）

--- PSP/PSP1.1/PSP1.1.cpp
+++ PSP/PSP1.1/PSP1.1.cpp
@@ -1,9 +1,8 @@
 /* ********************** */
-/* ファイル番号：３ */
-/* 日付：2019年 12月 28日*/
-/* プログラム内容：線形回帰のパラメータ、相関係数とその２乗値を求めるプログラム */
+/* ファイル番号：4 */
+/* 日付：2020年 5月 2日*/
+/* プログラム内容：５つの相対規模（VS,S,M,L,VL）を求める。 */
 /* ********************** */
-
 #include<iostream>
 #include<string>
 #include<fstream>
@@ -22,17 +21,85 @@ enum scales {
 	VL,
 };
 
+/**************************************************************/
+/* クラスCalculationRelativeScale */
+/* 役割：相対規模の算出 */
+/* 使用方法：相対規模を求めたい配列を指定し、インスタンスを作成した後メソッドを呼び出す。 */
+/***************************************************************/
+class CalculationRelativeScale // class begin
+{
+private:
+	vector<double> value;
+
+public:
+	CalculationRelativeScale(vector<double> valueX, vector<double> valueY)
+	{
+		for (unsigned int i = 0; i < valueX.size(); i++)
+		{
+			this->value.push_back(valueX[i] / valueY[i]);
+		}
+	}
+
+	~CalculationRelativeScale()
+	{
+
+	}
+
+	double findCalculationRelativeScale (enum scales scale) // method
+	{
+		vector<double> value_ln; // valueの自然対数値
+		for (unsigned int i = 0; i < value.size(); i++)
+		{
+			value_ln.push_back(log(value[i]));
+		}
+
+		mylib::Calculation *calculation_pointer = new mylib::Calculation(value_ln);
+		double average = calculation_pointer->findAverage();
+		double std = calculation_pointer->findSTD();
+
+		delete calculation_pointer;
+
+		double range_ln = 0.0; // 対数範囲
+		double range_size = 0.0; // 実際の範囲 
+		switch (scale)
+		{
+		case VS:
+			range_ln = average - 2 * std;
+			break;
+
+		case S:
+			range_ln = average - std;
+			break;
+			
+		case M:
+			range_ln = average;
+			break;
+
+		case L:
+			range_ln = average + std;
+			break;
+
+		case VL:
+			range_ln = average + 2 * std;
+			break;
+		}
+
+		range_size = exp(range_ln);
+		return range_size;
+	}
+}; // class end
+
 /*********************************************/
 /* メイン関数 */
 /* 宣言：int main(int argc, char *argv[]) */
-/* 処理内容：他の関数の呼び出し、クラスのインスタンス化、エラーチェックを行う */
-/* 引数：データセットが記載されたファイルの名前 */
+/* 処理内容：相対規模を算出・表示するため、必要なクラスや関数を呼び出す。エラーチェックも行う */
+/* 引数：データセットが記載されたファイルの名前、データセット内の変数の数 */
 /* 戻り値：常時０を返す */
 /*********************************************/
 int main (int argc, char *argv[]) // function begin
 {
 	mylib::Show *show_pointer;
-	if (argc != 2) // 指定されたファイルが１つでない場合
+	if (argc != 3) // 指定されたファイルが2つでない場合
 	{
 		vector<string> tmpstr{ "0" };
 		vector<double> tmpvalue{ 0 };
@@ -57,23 +124,22 @@ int main (int argc, char *argv[]) // function begin
 	vector<double> valueX;
 	vector<double> valueY;
 	scales scale;
-	mylib::FileReading::csvReading(argv[1], &valueX, &valueY, int(argv[2]));
-	if (int(argv[2]) == 1)
+	mylib::FileReading::csvReading(argv[1], &valueX, &valueY, atoi(argv[2]));
+	if (atoi(argv[2]) == 1)
 	{
 		valueY = vector<double>(valueX.size(), 1);
 	}
 
-	CalculationRelativeScale *crs_pointer = new CalculationRelativeScale(valueX, valueY);
+	CalculationRelativeScale* crs_pointer = new CalculationRelativeScale(valueX, valueY);
 
 	vector<string> string_display = { "VS", "S", "M", "L" , "VL"};
-	vector<double> value_display = 
-	{ 
-		crs_pointer->findCalculationRelativeScale(scale = VS), 
-		crs_pointer->findCalculationRelativeScale(scale = S), 
-		crs_pointer->findCalculationRelativeScale(scale = M), 
-		crs_pointer->findCalculationRelativeScale(scale = L), 
-		crs_pointer->findCalculationRelativeScale(scale = VL)
-	};
+	vector<double> value_display;
+	value_display.push_back(crs_pointer->findCalculationRelativeScale(scale = VS));
+	value_display.push_back(crs_pointer->findCalculationRelativeScale(scale = S));
+	value_display.push_back(crs_pointer->findCalculationRelativeScale(scale = M));
+	value_display.push_back(crs_pointer->findCalculationRelativeScale(scale = L));
+	value_display.push_back(crs_pointer->findCalculationRelativeScale(scale = VL));
+
 	delete crs_pointer;
 
 	show_pointer = new mylib::Show(string_display, value_display, 0);
@@ -83,66 +149,3 @@ int main (int argc, char *argv[]) // function begin
 
 	return 0;
 } // function end
-
-class CalculationRelativeScale
-{
-private:
-	vector<double> value;
-
-public:
-	CalculationRelativeScale(vector<double> valueX, vector<double> valueY)
-	{
-		for (unsigned int i = 0; i < valueX.size(); i++)
-		{
-			this->value.push_back(valueX[i] / valueY[i]);
-		}
-	}
-
-	~CalculationRelativeScale()
-	{
-
-	}
-
-	double findCalculationRelativeScale(enum scales scale)
-	{
-		vector<double> value_ln; // valueの自然対数値
-		for (unsigned int i = 0; i < value.size(); i++)
-		{
-			value_ln.push_back(log(value[i]));
-		}
-		
-		mylib::Calculation *calculation_pointer = new mylib::Calculation(value_ln);
-		double average = calculation_pointer->findAverage();
-		double std = calculation_pointer->findSTD();
-
-		delete calculation_pointer;
-
-		double range_ln; // 対数範囲
-		double range_size; // 実際の範囲 
-		switch (scale)
-		{
-		case VS:
-			range_ln = average - 2 * std;
-			break;
-
-		case S:
-			break;
-			range_ln = average - std;
-		
-		case M:
-			range_ln = average;
-			break;
-		
-		case L:
-			range_ln = average + std;
-			break;
-		
-		case VL:
-			range_ln = average + 2 * std;
-			break;
-		}
-
-		range_size = exp(range_ln);
-		return range_size;
-	}
-};
--- PSP/PSP1.1/mylib.h
+++ PSP/PSP1.1/mylib.h
@@ -5,14 +5,173 @@
 #include<map>
 #include<numeric>
 #include<cmath>
+#include<fstream>
+
 using namespace std;
 
 namespace mylib
 {
-	class FileReading
+	/**************************************************************/
+	/* クラスCalculation */
+	/* 役割：汎用的な計算処理を行う */
+	/* 使用方法：配列を指定し、インスタンスを作成した後メソッドを呼び出す。 */
+	/* 　　　　　指定した配列の平均値、分散、標準偏差の計算ができる。 */
+	/***************************************************************/
+	class Calculation // class begin
+	{
+	private:
+		vector<double> value;
+
+	public:
+		Calculation(vector<double> value)
+		{
+			this->value = value;
+		}
+
+		~Calculation()
+		{
+
+		}
+
+		double findAverage() // method
+		{
+			double sum = accumulate(value.begin(), value.end(), 0.0);
+			int num = value.size();
+			return sum / num;
+		}
+
+		double findCorrelation(vector<double> valueX, vector<double> valueY) // method
+		{
+			vector<double> productXY; // valueXとvalueYの積の集合
+			for (unsigned int i = 0; i < valueX.size(); i++)
+			{
+				productXY.push_back(valueX[i] * valueY[i]);
+			}
+			double n_sum_productXY = valueX.size() * accumulate(productXY.begin(), productXY.end(), 0.0);
+
+			double product_sumXY = accumulate(valueX.begin(), valueX.end(), 0.0) * accumulate(valueY.begin(), valueY.end(), 0.0); //valueXの和 * valueYの和
+
+			vector<double> valueX2; // valueXの2乗
+			for (unsigned int i = 0; i < valueX.size(); i++)
+			{
+				valueX2.push_back(valueX[i] * valueX[i]);
+			}
+			double n_sum_valueX2 = valueX.size() * accumulate(valueX2.begin(), valueX2.end(), 0.0); // n * valueXの2乗の和
+
+			double sum2_valueX = accumulate(valueX.begin(), valueX.end(), 0.0) * accumulate(valueX.begin(), valueX.end(), 0.0); //valueXの和の2乗
+
+			vector<double> valueY2; // valueYの2乗
+			for (unsigned int i = 0; i < valueY.size(); i++)
+			{
+				valueY2.push_back(valueY[i] * valueY[i]);
+			}
+			double n_sum_valueY2 = valueY.size() * accumulate(valueY2.begin(), valueY2.end(), 0.0); // n * valueYの2乗の和
+
+			double sum2_valueY = accumulate(valueY.begin(), valueY.end(), 0.0) * accumulate(valueY.begin(), valueY.end(), 0.0); //valueYの和の2乗
+
+			return (n_sum_productXY - product_sumXY) / sqrt((n_sum_valueX2 - sum2_valueX) * (n_sum_valueY2 - sum2_valueY));
+		}
+
+		double findVariance() // method
+		{
+			vector<double> deviation2; // 偏差の2乗値の集合
+			double average = findAverage();
+			for (unsigned int i = 0; i < value.size(); i++)
+			{
+				double d2 = pow(value[i] - average, 2); // 偏差の2乗値
+				deviation2.push_back(d2);
+			}
+			double sum_diff2 = accumulate(deviation2.begin(), deviation2.end(), 0.0);
+			int n = value.size();
+			return sum_diff2 / (n - 1);
+		}
+
+		double findSTD() // method
+		{
+			double variance = findVariance();
+			return sqrt(variance);
+		}
+	}; // class end
+
+	/**************************************************************/
+	/* クラスShow */
+	/* 役割：画面への出力を行う */
+	/* 使用方法：コンストラクタ引数に表示する文字列、表示する値を指定しインスタンス化した後、メソッドを呼び出す。 */
+	/* 　　　　　箇条書き形式の出力のほか、エラー検出時のエラーメッセージの表示ができる。 */
+	/***************************************************************/
+	class Show // class begin
+	{
+	private:
+		vector<string> string_display;
+		vector<double> value_display;
+		int error_flag;
+
+	public:
+		Show(vector<string> string_display, vector<double> value_display, int error_flag)
+		{
+			this->string_display = string_display;
+			this->value_display = value_display;
+			this->error_flag = error_flag;
+		}
+
+		~Show()
+		{
+
+		}
+
+		void displayAsItemzation() // method
+		{
+			if (error_flag == 0) 
+			{
+				for (unsigned int i = 0; i < string_display.size(); i++)
+				{
+					cout << string_display[i] << " = " << value_display[i] << endl;
+				}
+			}
+			return;
+		}
+
+		void displayAsError() // method
+		{
+			if (error_flag != 0)
+			{
+				map<int, string> error_message;
+				error_message[1] = "１つのファイルを指定してください";
+				error_message[2] = "１つのファイルを指定してください";
+				error_message[3] = "ファイルのフォーマットが異なります";
+				error_message[4] = "ファイルが見つかりません";
+				error_message[5] = "ファイルに無効な値があります";
+
+				cout << "エラー番号：" << error_flag << endl;
+				cout << error_message[error_flag] << endl;
+			}
+			return;
+		}
+	}; // class end
+
+	/*********************************************/
+	/* 関数getFormat */
+	/* 宣言：void getFormat(string file_name) */
+	/* 処理内容：ファイル名から拡張子を調べる */
+	/* 引数：第１引数→ファイル名 */
+	/* 戻り値：拡張子名（ドット（.）も含む） */
+	/*********************************************/
+	string getFormat (string file_name) // function begin
+	{
+		int format_index = file_name.find_last_of(".");
+		string format_name = file_name.substr(format_index, file_name.size() - format_index);
+		return format_name;
+	} // function end
+
+	/**************************************************************/
+	/* クラスFileReading */
+	/* 役割：ファイルを開き、値を指定した変数に格納する */
+	/* 使用方法： 開くファイル名、格納する変数のアドレスを指定し、拡張子に対応したメソッドを呼び出す。 */
+	/***************************************************************/
+	class FileReading 
 	{
 	public:
-		static void csvReading(string file_name, vector<double>* valueX, vector<double>* valueY, int param)
+		static void csvReading(string file_name, vector<double>* valueX, vector<double>* valueY, int param) // method
 		{
 			ifstream reading_file(file_name, ios::in);
 			mylib::Show *show_pointer;
@@ -86,150 +245,4 @@ namespace mylib
 			return;
 		}
 	};
-
-	/**************************************************************/
-	/* クラスCalculation */
-	/* 役割：複数の値からさまざまな計算処理を行う */
-	/* 使用方法：コンストラクタ引数に値が格納された変数を指定しインスタンス化した後、メソッドを呼び出す。 */
-	/* 　　　　　線形回帰のパラメータ、平均値、相関係数などの計算ができる。 */
-	/***************************************************************/
-	class Calculation // class begin
-	{
-	private:
-		vector<double> value;
-
-	public:
-		Calculation(vector<double> value)
-		{
-			this->value = value;
-		}
-
-		~Calculation()
-		{
-
-		}
-
-		double findAverage() // method
-		{
-			double sum = accumulate(value.begin(), value.end(), 0.0);
-			int num = value.size();
-			return sum / num;
-		}
-
-		double findCorrelation(vector<double> valueX, vector<double> valueY) // method
-		{
-			vector<double> productXY; // valueXとvalueYの積の集合
-			for (unsigned int i = 0; i < valueX.size(); i++)
-			{
-				productXY.push_back(valueX[i] * valueY[i]);
-			}
-			double n_sum_productXY = valueX.size() * accumulate(productXY.begin(), productXY.end(), 0.0);
-
-			double product_sumXY = accumulate(valueX.begin(), valueX.end(), 0.0) * accumulate(valueY.begin(), valueY.end(), 0.0); //valueXの和 * valueYの和
-
-			vector<double> valueX2; // valueXの2乗
-			for (unsigned int i = 0; i < valueX.size(); i++)
-			{
-				valueX2.push_back(valueX[i] * valueX[i]);
-			}
-			double n_sum_valueX2 = valueX.size() * accumulate(valueX2.begin(), valueX2.end(), 0.0); // n * valueXの2乗の和
-
-			double sum2_valueX = accumulate(valueX.begin(), valueX.end(), 0.0) * accumulate(valueX.begin(), valueX.end(), 0.0); //valueXの和の2乗
-
-			vector<double> valueY2; // valueYの2乗
-			for (unsigned int i = 0; i < valueY.size(); i++)
-			{
-				valueY2.push_back(valueY[i] * valueY[i]);
-			}
-			double n_sum_valueY2 = valueY.size() * accumulate(valueY2.begin(), valueY2.end(), 0.0); // n * valueYの2乗の和
-
-			double sum2_valueY = accumulate(valueY.begin(), valueY.end(), 0.0) * accumulate(valueY.begin(), valueY.end(), 0.0); //valueYの和の2乗
-
-			return (n_sum_productXY - product_sumXY) / sqrt((n_sum_valueX2 - sum2_valueX) * (n_sum_valueY2 - sum2_valueY));
-		}
-
-		double findVariance()
-		{
-			vector<double> deviation2; // 偏差の2乗値の集合
-			double average = findAverage();
-			for (unsigned int i = 0; i < value.size(); i++)
-			{
-				double d2 = pow(value[i] - average, 2); // 偏差の2乗値
-				deviation2.push_back(d2);
-			}
-			double sum_diff2 = accumulate(deviation2.begin(), deviation2.end(), 0.0);
-			int n = value.size();
-			return sum_diff2 / (n - 1);
-		}
-
-		double findSTD()
-		{
-			double variance = findVariance();
-			return sqrt(variance);
-		}
-	}; // class end
-
-	/**************************************************************/
-	/* クラスShow */
-	/* 役割：画面への出力を行う */
-	/* 使用方法：コンストラクタ引数に表示する文字列、表示する値を指定しインスタンス化した後、メソッドを呼び出す。 */
-	/* 　　　　　箇条書き形式の出力のほか、エラー検出時のエラーメッセージの表示ができる。 */
-	/***************************************************************/
-	class Show // class begin
-	{
-	private:
-		vector<string> string_display;
-		vector<double> value_display;
-		int error_flag;
-
-	public:
-		Show(vector<string> string_display, vector<double> value_display, int error_flag)
-		{
-			this->string_display = string_display;
-			this->value_display = value_display;
-			this->error_flag = error_flag;
-		}
-
-		~Show()
-		{
-
-		}
-
-		void displayAsItemzation() // method
-		{
-			if (error_flag == 0) 
-			{
-				for (unsigned int i = 0; i < string_display.size(); i++)
-				{
-					cout << string_display[i] << " = " << value_display[i] << endl;
-				}
-			}
-			return;
-		}
-
-		void displayAsError() // method
-		{
-			if (error_flag != 0)
-			{
-				map<int, string> error_message;
-				error_message[1] = "１つのファイルを指定してください";
-				error_message[2] = "１つのファイルを指定してください";
-				error_message[3] = "ファイルのフォーマットが異なります";
-				error_message[4] = "ファイルが見つかりません";
-				error_message[5] = "ファイルに無効な値があります";
-
-				cout << "エラー番号：" << error_flag << endl;
-				cout << error_message[error_flag] << endl;
-			}
-			return;
-		}
-	}; // class end
-
-
-	string getFormat(string file_name)
-	{
-		int format_index = file_name.find_last_of(".");
-		string format_name = file_name.substr(format_index, file_name.size() - format_index);
-		return format_name;
-	}
 }
